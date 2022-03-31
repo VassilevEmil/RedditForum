0e96c4c6a3367a386f04a1fda1de9a7646673adc
VassilevEmil <84077074+VassilevEmil@users.noreply.github.com>
2022-03-31 19:35:21 +0200

    REST API modules added, as well as application layer. The structure is now corrected and only needs to be properly implemented

---
+++ RESTClient/Class1.cs
@@ -0,0 +1,5 @@
+﻿namespace RESTClient;
+
+public class Class1
+{
+}
---
+++ WebAPI/Controllers/UserController.cs
@@ -0,0 +1,31 @@
+﻿using Domain.Contracts;
+using Domain.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace WebAPI.Controllers;
+
+
+[ApiController]
+[Route("[controller]")]
+public class UserController : ControllerBase
+{
+    private IUserService userService;
+
+    public UserController(IUserService userService)
+    {
+        this.userService = userService;
+    }
+    
+    //get all users
+    /*
+    [HttpGet]
+    public async Task<ActionResult<ICollection<User>>> GetALl()
+    {
+        try
+        {
+            ICollection<User> users = await userService.GetUserAsync()
+        }
+    }
+    */
+
+}
